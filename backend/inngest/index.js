069f61088ee528307fe837cb7518510d43d20393
Harshith N <harshith1440@gmail.com>
2025-10-27 20:27:24 +0530

    userUpdation fixed

--- backend/inngest/index.js
+++ backend/inngest/index.js
@@ -49,4 +49,4 @@ const syncUserUpdation = inngest.createFunction(
 
 
 // Create an empty array where we'll export future Inngest functions
-export const functions = [syncUserCreation,syncUserDeletion];
+export const functions = [syncUserCreation,syncUserDeletion,syncUserUpdation];
