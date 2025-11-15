eba147236bd6b08f6a5a6a1156fb1f3fb4359a7e
Harshith N <harshith1440@gmail.com>
2025-11-15 21:12:01 +0530

    email corrected

--- backend/configs/nodeMailer.js
+++ backend/configs/nodeMailer.js
@@ -1,17 +1,18 @@
 import nodemailer from 'nodemailer'
+
 // Create a transporter for SMTP
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
   port: 587,
   auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
+    user: process.env.SMTP_USER, // Make sure this is set in Vercel
+    pass: process.env.SMTP_PASS, // Make sure this is set in Vercel
   },
 });
 
 const sendEmail = async({to,subject,body})=>{
-    const response = await transporter.sendEmail({
-        from : process.env.SENDER_EMAIL,
+    const response = await transporter.sendMail({ // ⭐ FIX: Changed to sendMail
+        from : process.env.SENDER_EMAIL, // Make sure this is set in Vercel
         to,
         subject,
         html : body,
