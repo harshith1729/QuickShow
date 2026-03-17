4576f6a2d7e9eea8d172baee6341fd39401cb7a0
Harshith N <harshith1440@gmail.com>
2026-03-17 22:18:15 +0530

    correction2

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
-app.options("*", cors());
+app.options("/(.*)", cors());
 
 // Stripe webhook (must be BEFORE express.json)
 app.post(
